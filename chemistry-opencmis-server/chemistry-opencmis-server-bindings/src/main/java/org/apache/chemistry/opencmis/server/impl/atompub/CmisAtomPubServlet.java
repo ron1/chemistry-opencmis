e5f53b94a98ac27241bf82335162bc8a162257fd
ron1 <ron1@localhost>
2014-09-25 04:21:16 -0400

    Revert "CMIS-811 Support HTTP HEAD Method"
    This reverts commit 6139919ac02c0b933502a3701596af3117fec25d.

--- chemistry-opencmis-server/chemistry-opencmis-server-bindings/src/main/java/org/apache/chemistry/opencmis/server/impl/atompub/CmisAtomPubServlet.java
+++ chemistry-opencmis-server/chemistry-opencmis-server-bindings/src/main/java/org/apache/chemistry/opencmis/server/impl/atompub/CmisAtomPubServlet.java
@@ -75,9 +75,7 @@ import org.apache.chemistry.opencmis.server.impl.ServerVersion;
 import org.apache.chemistry.opencmis.server.shared.AbstractCmisHttpServlet;
 import org.apache.chemistry.opencmis.server.shared.Dispatcher;
 import org.apache.chemistry.opencmis.server.shared.ExceptionHelper;
-import org.apache.chemistry.opencmis.server.shared.HEADHttpServletRequestWrapper;
 import org.apache.chemistry.opencmis.server.shared.HttpUtils;
-import org.apache.chemistry.opencmis.server.shared.NoBodyHttpServletResponseWrapper;
 import org.apache.chemistry.opencmis.server.shared.QueryStringHttpServletRequestWrapper;
 import org.apache.chemistry.opencmis.server.shared.ServiceCall;
 import org.apache.commons.lang.StringEscapeUtils;
@@ -162,6 +160,7 @@ public class CmisAtomPubServlet extends AbstractCmisHttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
+        QueryStringHttpServletRequestWrapper qsRequest = new QueryStringHttpServletRequestWrapper(request);
 
         // set default headers
         response.addHeader("Cache-Control", "private, max-age=0");
@@ -170,17 +169,8 @@ public class CmisAtomPubServlet extends AbstractCmisHttpServlet {
         // create a context object, dispatch and handle exceptions
         CallContext context = null;
         try {
-            String method = request.getMethod();
-
-            if (METHOD_HEAD.equals(method)) {
-                request = new HEADHttpServletRequestWrapper(request);
-                response = new NoBodyHttpServletResponseWrapper(response);
-            } else {
-                request = new QueryStringHttpServletRequestWrapper(request);
-            }
-
-            context = createContext(getServletContext(), request, response);
-            dispatch(context, request, response);
+            context = createContext(getServletContext(), qsRequest, response);
+            dispatch(context, qsRequest, response);
         } catch (Exception e) {
             if (e instanceof CmisUnauthorizedException) {
                 response.setHeader("WWW-Authenticate", "Basic realm=\"CMIS\"");
